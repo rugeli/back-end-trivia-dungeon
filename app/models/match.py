f087c630e8ad1db2fdc687c230367540766c5cdc
Ruge Li <rugeli0605@gmail.com>
2022-07-28 21:41:40 -0700

    working on schema design

---
+++ app/models/match.py
@@ -0,0 +1,4 @@
+from app import db
+
+class Match(db.Model):
+    
--- app/models/question.py
+++ app/models/question.py
@@ -4,5 +4,5 @@ class Question(db.Model):
     question_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.String)
     correct_answer = db.Column(db.String)
-    options = db.Column(db.List)
+    incorrect_answers = db.Column(db.List)
     category = db.Column(db.String)
