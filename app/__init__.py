eeb2d1f6f0b3903efa93edfb1fdd33a6aa798ecc
Ruge Li <rugeli0605@gmail.com>
2022-08-15 15:41:06 -0700

    for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# from flaskext.mysql import MySQL
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
@@ -7,6 +8,7 @@ from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
+# mysql = MySQL()
 load_dotenv()
 
 #holds configurations for the application 
@@ -15,6 +17,12 @@ def create_app():
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #part of setting 
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
                 "SQLALCHEMY_DATABASE_URI")
+
+        # app.config['MYSQL_DATABASE_USER'] = 'bb6b6b599e0afe4'
+        # app.config['MYSQL_DATABASE_PASSWORD'] = '0428810e'
+        # app.config['MYSQL_DATABASE_DB'] = 'heroku_7d82b560bf0c60d'
+        # app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
+        # mysql.init_app(app)
 
 
         from app.models.user import User
