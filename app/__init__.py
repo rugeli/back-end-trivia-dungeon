53c89d59267f0850a379efab7b746cc2665092d4
Ruge Li <rugeli0605@gmail.com>
2022-08-16 15:24:19 -0700

    add engine options

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flaskext.mysql import MySQL
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
@@ -8,7 +7,6 @@ from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
-# mysql = MySQL()
 load_dotenv()
 
 #holds configurations for the application 
@@ -18,12 +16,12 @@ def create_app():
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #part of setting 
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
                 "SQLALCHEMY_DATABASE_URI")
+        app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+                "pool_pre_ping": True,
+                "pool_recycle": 300,
+                }
+        
 
-        # app.config['MYSQL_DATABASE_USER'] = 'bb6b6b599e0afe4'
-        # app.config['MYSQL_DATABASE_PASSWORD'] = '0428810e'
-        # app.config['MYSQL_DATABASE_DB'] = 'heroku_7d82b560bf0c60d'
-        # app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
-        # mysql.init_app(app)
 
 
         from app.models.user import User
