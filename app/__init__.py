96d735ccd840925474fbf7d048f3bf2c3f57ff68
Ruge Li <rugeli0605@gmail.com>
2022-08-16 13:16:29 -0700

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flaskext.mysql import MySQL
+from flaskext.mysql import MySQL
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
@@ -8,21 +8,22 @@ from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
-# mysql = MySQL()
+mysql = MySQL()
 load_dotenv()
 
 #holds configurations for the application 
 def create_app():
         app = Flask(__name__)
+        CORS(app)
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #part of setting 
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
                 "SQLALCHEMY_DATABASE_URI")
 
-        # app.config['MYSQL_DATABASE_USER'] = 'bb6b6b599e0afe4'
-        # app.config['MYSQL_DATABASE_PASSWORD'] = '0428810e'
-        # app.config['MYSQL_DATABASE_DB'] = 'heroku_7d82b560bf0c60d'
-        # app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
-        # mysql.init_app(app)
+        app.config['MYSQL_DATABASE_USER'] = 'bb6b6b599e0afe4'
+        app.config['MYSQL_DATABASE_PASSWORD'] = '0428810e'
+        app.config['MYSQL_DATABASE_DB'] = 'heroku_7d82b560bf0c60d'
+        app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
+        mysql.init_app(app)
 
 
         from app.models.user import User
@@ -38,5 +39,4 @@ def create_app():
         from .question_routes import questions_bp
         app.register_blueprint(questions_bp)
 
-        CORS(app)
         return app
